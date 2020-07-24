3c7a3989cc8a05ba7de77c2542cef073b7caac28
이호섭 <62980969+hoossky@users.noreply.github.com>
2020-07-24 19:08:55 +0900

    "19:08"

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import {Provider} from 'react-redux'
 import store from "./helpers/store";
 import * as serviceWorker from './serviceWorker';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 
 ReactDOM.render(
